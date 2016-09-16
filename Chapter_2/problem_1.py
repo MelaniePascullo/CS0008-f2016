6b4eb2c3369eac740ab00407cb3caf1963b90533
Melanie Pascullo <mgp26@pitt.edu>
2016-09-15 21:59:26 -0400

    committing problem 1

---
+++ Chapter_2/problem_1.py
@@ -0,0 +1,9 @@
+name = "Melanie" 
+address = "%s,%s,%s," % ("Jackson", "New Jersey", "08527")
+phone_number = "7329420604"
+major = "neuroscience"
+print "my name is", name
+print "my address is", address
+print "my phone number is", phone_number
+print "my major is", major
+ 
