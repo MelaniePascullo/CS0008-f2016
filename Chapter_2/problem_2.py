1d23d4be6e8de6f5c77d69c381a5bb963e7a91a7
MelaniePascullo <MGP26@pitt.edu>
2016-10-19 15:14:58 -0400

    Chapter 2 problem 2
    Printed total profits of company

---
+++ Chapter_2/problem_2.py
@@ -0,0 +1,7 @@
+# Define conversion value
+total_sales = 0.23
+# We ask the user what the projected sales are
+sales = float(input("What is the projected number of total sales?"))
+profit = sales * total_sales
+# Tell user their total profit
+print("Your total profit is $" + str(float(profit)))
