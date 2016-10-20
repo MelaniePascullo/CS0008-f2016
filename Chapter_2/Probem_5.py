5ac1e2a874005df3053a7cb42fabfa76ea1a223f
MelaniePascullo <MGP26@pitt.edu>
2016-10-20 15:34:57 -0400

    Chapter 2 problem 5
    Printing distance traveled based on three different times given.

---
+++ Chapter_2/Probem_5.py
@@ -0,0 +1,11 @@
+# Set variable for how fast the car is traveling
+distance = 70
+# Create formulas for how far each car will travel based on speed and time (distance = speeed * time)
+distance_1 = 70 * 6
+distance_2 = 70 * 10
+distance_3 = 15
+# Let user know how fast the car is traveling
+print("If the car is traveling at 70 miles per hour...")
+print("After 6 hours the car will travel " + str(float(distance_1)) + " miles")
+print("After 10 hours the car will travel " + str(float(distance_2)) + " miles")
+print("After 15 hours the car will travel " + str(float(distance_3)) + " miles")
