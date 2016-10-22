a0e4124e5360d9236ded93f052fd0a9429219c45
MelaniePascullo <MGP26@pitt.edu>
2016-10-21 21:31:33 -0400

    Problem 3
    Age classifier

---
+++ Chapter-3/Problem_3.py
@@ -0,0 +1,9 @@
+age = int(input("Please enter your age"))
+if age <= 1:
+    print("You are an infant!")
+elif age <= 13:
+    print("You are a child!")
+elif age <= 20:
+    print("You are a teenager!")
+else:
+    print("You are an adult!")
