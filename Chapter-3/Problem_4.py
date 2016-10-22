c8a4fdf2b7addad2431c3386ac7b6171844cf8bd
MelaniePascullo <MGP26@pitt.edu>
2016-10-21 21:32:03 -0400

    Problem 4
    Roman numerals

---
+++ Chapter-3/Problem_4.py
@@ -0,0 +1,23 @@
+roman = int(input("Enter a number 1-10 to recieve the roman numeral version"))
+if roman == 1:
+    print("Your roman numeral is I")
+elif roman == 2:
+    print("Your roman numeral is II")
+elif roman == 3:
+    print("Your roman numeral is III")
+elif roman == 4:
+    print("Your roman numeral is IV")
+elif roman == 5:
+    print("Your roman numeral is V")
+elif roman == 6:
+    print("Your roman numbral is VI")
+elif roman == 7:
+    print("Your roman numeral is VII")
+elif roman == 8:
+    print("Your roman numeral is VIII")
+elif roman == 9:
+    print("Your roman numeral is IX")
+elif roman == 10:
+    print("Your roman numberal is X")
+else:
+    print("Invalid number")
